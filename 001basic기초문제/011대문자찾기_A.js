40aca87f8b9de2a80a56a1f76439414d99afc809
AppleTrick <arrans9591@gmail.com>
2021-09-08 18:44:40 +0900

    대문자 찾기

--- 001basic기초문제/011대문자찾기_A.js
+++ 001basic기초문제/011대문자찾기_A.js
@@ -4,10 +4,13 @@
 한 개의 문자열을 입력받아 해당 문자열에 알파벳 대문자가 몇 개 있는지 알아내는 프로그램 을 작성하세요.
 */
 
+// charCodeAt을 이용해서 푸는 방법도 있다.
 function solution(str){
     let answer = 0;
+    let num = 0;
     for (const x of str) {
-        if(x === x.toUpperCase()){
+        num = x.charCodeAt();
+        if(num >= 65 && num <= 90){
             answer++;
         }
     }
