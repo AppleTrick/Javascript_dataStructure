4ee40164ebf1fff798d04701af0f7bee1613cfd9
AppleTrick <arrans9591@gmail.com>
2021-09-10 20:56:36 +0900

    k번째 큰수

--- 004완전탐색/005K번째큰수.js
+++ 004완전탐색/005K번째큰수.js
@@ -5,3 +5,21 @@ K번째 큰 수
 
 만약 큰 수부터 만들어진 수가 25 25 23 23 22 20 19......이고 K값이 3이라면 K번째 큰 값 은 22입니다.
 */
+
+function solution(n, k, card){
+    let set = new Set();
+    for (let i = 0; i < n; i++) {
+       for (let j = i+1; j < n; j++) {
+          for (let l = j+1; l < n; l++){
+              set.add(card[i]+card[j]+card[l]);
+          } 
+       }
+    }
+    let result = Array.from(set).sort((a,b) => {
+        return b-a;
+    })
+    return result[k-1];
+}
+
+let arr=[13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+console.log(solution(10, 3, arr));
