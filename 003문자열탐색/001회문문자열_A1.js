0684d901380a093e0d16213b1a3279ebf965967e
AppleTrick <arrans9591@gmail.com>
2021-09-10 18:55:09 +0900

    유효한 팰린드롬, 회문문자열

--- 003문자열탐색/001회문문자열_A1.js
+++ 003문자열탐색/001회문문자열_A1.js
@@ -15,7 +15,7 @@ function solution(str){
     let len = str.length;
     for (let i = 0; i < Math.floor(len/2); i++) {
         if(s[i]!==s[len-i-1]){
-
+            return "NO";
         }
     }
     return answer;
