db4b8085cacc8eca3f9db5f0e4a182635f9bd73b
Changhee Park <arrans9591@gmail.com>
2021-09-11 13:23:47 +0900

    재귀함수와 스택프레임

---
+++ 008재귀함수와완전탐색/001재귀함수와스택프레임.js
@@ -0,0 +1,21 @@
+/*
+재귀함수
+자연수 N이 입력되면 재귀함수를 이용하여 1부터 N까지를 출력하는 프로그램을 작성하세요.
+
+*/
+
+function solution(n){
+
+    function DFS(L){
+        if(L==0){
+            return;
+        }else{
+            console.log(L);
+            DFS(L-1)
+        }
+    }
+
+    DFS(n)
+}
+
+solution(3);
